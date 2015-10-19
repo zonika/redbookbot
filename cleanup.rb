599a6215971afdc381d90d138432c2b4a4454107
zonika <zchodosh@conncoll.edu>
2015-10-19 16:17:42 -0400

    take out keys

--- cleanup.rb
+++ cleanup.rb
@@ -6,13 +6,11 @@ class TwitterApi
   attr_reader :client
 
   def initialize
-    keys = YAML.load_file('application.yml')
-
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = keys['CONSUMER_KEY']
-      config.consumer_secret     = keys['CONSUMER_SECRET']
-      config.access_token        = keys['ACCESS_TOKEN']
-      config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
     end
 
     @a = []
